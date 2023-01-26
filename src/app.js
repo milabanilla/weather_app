6824d54359fb6a7c18110f78abd1ef29ba09b3c1
Mila <yamila_meza_@outlook.com>
2023-01-26 10:04:35 -0800

    Removed a block of html and injected it into javascript function

--- src/app.js
+++ src/app.js
@@ -20,7 +20,33 @@ function formatDate(timestamp){
   let day = days[date.getDay()]
   return `${day}  ${hours}:${minutes}`
 }
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
 
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML + `
+      <div class="col-2">
+        <div class="weather-forecast-date">${day}</div>
+        <img
+          src="http://openweathermap.org/img/wn/50d@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> 18° </span>
+          <span class="weather-forecast-temperature-min"> 12° </span>
+        </div>
+      </div>
+  `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+
+}
 function displayTemperature(response){
   let tempertaureElement = document.querySelector('#temperature');
   let cityElement = document.querySelector('#city');
@@ -81,7 +107,6 @@ function currentPosition(position) {
   let lon = position.coords.longitude;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
   let showCurrentPosition = document.querySelector("#city");
-  showCurrentPosition.innerHTML = `Latitude: ${lat} Longtitude:${lon}`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
@@ -102,4 +127,6 @@ let fahrenheitLink = document.querySelector("#fahrenheit-link");
 fahrenheitLink.addEventListener("click",displayFahrenheitTemperature)
 
 let celciusLink = document.querySelector("#celcius-link")
-celciusLink.addEventListener('click',displayCelciusTemperature)
+celciusLink.addEventListener('click',displayCelciusTemperature);
+
+displayForecast();
