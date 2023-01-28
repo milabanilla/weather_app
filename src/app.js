9df52ab677b28bfb2482dd5576f51963d7b0779b
Mila <yamila_meza_@outlook.com>
2023-01-28 12:15:21 -0800

    Added forecast weather and api integration

--- src/app.js
+++ src/app.js
@@ -1,51 +1,70 @@
-function formatDate(timestamp){
-  //calculate the date
-  let date = new Date(timestamp)
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
   let hours = date.getHours();
-  if (hours <10){
-      hours=`0${hours}`
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
   let minutes = date.getMinutes();
-  if (minutes <10){
-      minutes=`0${minutes}`
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
+
   let days = [
-      "Sunday", 
-      "Monday",
-      "Tuesday", 
-      "Wednesday",
-      "Thursday", 
-      "Friday",
-      "Saturday"];
-  let day = days[date.getDay()]
-  return `${day}  ${hours}:${minutes}`
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
-  let days = ["Thu", "Fri", "Sat", "Sun"];
-
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML = forecastHTML + `
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
         <img
-          src="http://openweathermap.org/img/wn/50d@2x.png"
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
           alt=""
           width="42"
         />
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max"> 18° </span>
-          <span class="weather-forecast-temperature-min"> 12° </span>
+          <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+          <span class="weather-forecast-temperature-min">  ${Math.round(forecastDay.temp.min)}° </span>
         </div>
       </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
 
+function getForecast(coordinates) {
+  let apiKey = "0efb4fc16a9ed98dc0b3aafd8491d6ad";
+  let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiURL).then(displayForecast);
 }
 function displayTemperature(response){
   let tempertaureElement = document.querySelector('#temperature');
@@ -69,7 +88,7 @@ function displayTemperature(response){
   iconElement.setAttribute('src', `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
   //to change the alt, use the same method
   iconElement.setAttribute('alt', response.data.weather[0].description)
-
+  getForecast(response.data.coord)
 }
 function search(city){
   let apiKey= "0efb4fc16a9ed98dc0b3aafd8491d6ad"
@@ -128,5 +147,3 @@ fahrenheitLink.addEventListener("click",displayFahrenheitTemperature)
 
 let celciusLink = document.querySelector("#celcius-link")
 celciusLink.addEventListener('click',displayCelciusTemperature);
-
-displayForecast();
